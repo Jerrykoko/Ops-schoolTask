ed2d0af0d8619e154a53114def5be5f09559b513
jerrykoko <integritygerald@gmail.com>
2021-07-24 15:24:06 +0200

    initial commit

--- vagrant/fixScripts/exercise1-fix.sh
+++ vagrant/fixScripts/exercise1-fix.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
-#add fix to exercise1 here
+vagrant ssh server1
+sudo ip route del 208.86.224.90 via 192.168.100.10 dev enp0s8
+sudo ip link set dev enp0s8 down
+sudo ip link set dev enp0s8 up
